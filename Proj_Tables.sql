d39b0be007420aea3c8a4d9ef49f8253f2bfaad4
infantej2 <jethro.ynfante@gmail.com>
2023-01-18 14:52:11 -0700

    Completed table

--- Proj_Tables.sql
+++ Proj_Tables.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE Department (
     d_id INT PRIMARY KEY,
-    building VARCHAR(255)
+    building INT
 );
 
 CREATE TABLE Instructor (
@@ -27,6 +27,12 @@ CREATE TABLE Student (
     gender VARCHAR(255)
 ); 
 
+CREATE TABLE Timeslot (
+	ts_id INT PRIMARY KEY,
+	start_time TIME,
+	end_time TIME,
+);
+
 CREATE TABLE Section (
 	sec_id INT,
 	c_id INT,
@@ -36,8 +42,30 @@ CREATE TABLE Section (
 	year INT,
 	capacity INT,
 	enrolled INT,
-	PRIMARY KEY (sec_id, c_id, ts_id, i_id, semester, year),
-	FOREIGN KEY (c_id) REFERENCES Course (c_id),
-	FOREIGN KEY (ts_id) REFERENCES Timeslot (ts_id),
-	FOREIGN KEY (i_id) REFERENCES Instructor (i_id)
+    PRIMARY KEY (sec_id, c_id, ts_id, i_id, semester, year),
+	FOREIGN KEY (c_id) REFERENCES Course(c_id),
+	FOREIGN KEY (ts_id) REFERENCES Timeslot(ts_id),
+	FOREIGN KEY (i_id) REFERENCES Instructor(i_id)
 );
+
+
+CREATE TABLE Takes (
+	sec_id INT,
+	c_id INT,
+	ts_id INT,
+	i_id INT,
+	semester CHAR(3),
+	year INT,
+	s_id INT,
+	PRIMARY KEY (sec_id, c_id, ts_id, i_id, semester, year, s_id),
+	FOREIGN KEY (s_id) REFERENCES Student(s_id),
+	FOREIGN KEY (sec_id, c_id, ts_id, i_id, semester, year) REFERENCES Section(sec_id, c_id, ts_id, i_id, semester, year)
+);
+
+
+CREATE TABLE Prereq (
+	c_id INT PRIMARY KEY,
+	title VARCHAR(255),
+	credits INT
+);
+
