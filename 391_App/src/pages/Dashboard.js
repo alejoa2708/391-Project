61205c01874ebe300b5da59aada8377ae00f4c08
infantej2 <jethro.ynfante@gmail.com>
2023-02-03 19:32:01 -0700

    Playing around with tables and cleaned up

--- 391_App/src/pages/Dashboard.js
+++ 391_App/src/pages/Dashboard.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { IonButton, IonContent, IonGrid, IonIcon, IonLabel, IonList, IonPage, IonRow, IonToolbar, useIonAlert } from '@ionic/react';
 import { gridOutline, imageOutline, peopleOutline, basketballOutline, homeOutline, closeOutline, chevronForward, menuOutline, powerOutline } from 'ionicons/icons';
 import ReactDOM from 'react-dom';
 import { useHistory } from "react-router-dom";
 import './Dashboard.css';
-import Helper from '../helper';
-import { TableGrid } from 'ionic-react-tablegrid';
-import { DataGrid } from '@mui/x-data-grid';
+import TabOne from './TabOne.js';
+import TabTwo from './TabTwo.js';
 
 const Dashboard = () => {
 	
@@ -14,38 +13,31 @@ const Dashboard = () => {
 	const [layoutItems, setLayout] = useState([]);  // layout item holder
 	const [sidebar, setSidebar] = useState(false);	// Sets nav side bar state to false (closed)
 	const showSidebar = () => setSidebar(!sidebar); // Triggering sidebar to expand
-	const [students, setStudents] = useState({});
-	const [rows, setRows] = useState([]);
+	const [tab1, setTab1] = useState(true);
+	const [tab2, setTab2] = useState(false);
+	const [tab3, setTab3] = useState(false);
 	const history = useHistory();
-
-	useEffect(() => {
-		Helper.post(Helper.getAPIUrl('getStudents'), {}).then(response => {
-			//console.log(response.data.recordsets[0]);
-			setStudents(response.data.recordsets[0]);
-		});
-	}, []);
-
-	useEffect(() => {
-		if(students.length > 0) {
-			const data = [];
-			students.map((item) => 
-				data.push({ id: item.s_id, first_name: item.first_name, last_name: item.last_name, gender: item.gender })
-			);
-			setRows(data);
-		}
-	}, [students])
-
-	
-
-	const columns = [
-		{ field: 'id', headerName: 'ID', width: 150 },
-		{ field: 'first_name', headerName: 'First Name', width: 150 },
-		{ field: 'last_name', headerName: 'Last Name', width: 150 },
-		{ field: 'gender', headerName: 'Gender', width: 150 },
-	]
 
 	function onSelectMenu (elementId){
 		document.getElementById(elementId)?.setAttribute("id", "menu-items1");
+	}
+
+	function tabSwitch (tabNum){
+		if(tabNum == 1) {
+			setTab1(true);
+		} else {
+			setTab1(false);
+		}
+		if(tabNum == 2) {
+			setTab2(true);
+		} else {
+			setTab2(false);
+		}
+		if(tabNum == 3) {
+			setTab3(true);
+		} else {
+			setTab3(false);
+		}
 	}
 
 	/**
@@ -137,27 +129,27 @@ const Dashboard = () => {
 
 							<IonList id='menu-list' color= "dark">
 								
-								<IonButton className='menu-items' onClick={() => { }}>
+								<IonButton className='menu-items' onClick={() => { tabSwitch(1) }}>
 									<IonIcon className={getWindowDimensions() < 768 ? 'menu-icons-mobile': 'menu-icons'} icon={homeOutline} size='small'></IonIcon>	
 									<IonLabel className={sidebar ? 'menu-labels' : 'menu-labels collapsed' }>Dashboard</IonLabel>
 								</IonButton>
 
-								<IonButton className='menu-items' onClick={() => { }}>
+								<IonButton className='menu-items' onClick={() => { tabSwitch(2) }}>
 									<IonIcon className={getWindowDimensions() < 768 ? 'menu-icons-mobile': 'menu-icons'} icon={peopleOutline} size='small'></IonIcon>	
 									<IonLabel className={sidebar ? 'menu-labels' : 'menu-labels collapsed' }>Users</IonLabel>
 								</IonButton>
 
-								<IonButton className='menu-items' onClick={() => { }}>
+								<IonButton className='menu-items' onClick={() => { tabSwitch(3) }}>
 									<IonIcon className={getWindowDimensions() < 768 ? 'menu-icons-mobile': 'menu-icons'} icon={gridOutline} size='small'></IonIcon>	
 									<IonLabel className={sidebar ? 'menu-labels' : 'menu-labels collapsed' }>Courses</IonLabel>
 								</IonButton>
 
-								<IonButton className='menu-items' onClick={() => { }}>
+								<IonButton className='menu-items' onClick={() => { tabSwitch(4) }}>
 									<IonIcon className={getWindowDimensions() < 768 ? 'menu-icons-mobile': 'menu-icons'} icon={imageOutline} size='small'></IonIcon>	
 									<IonLabel className={sidebar ? 'menu-labels' : 'menu-labels collapsed' }>Lorem</IonLabel>
 								</IonButton>
 
-								<IonButton className='menu-items' onClick={() => { }}>
+								<IonButton className='menu-items' onClick={() => { tabSwitch(5) }}>
 									<IonIcon className={getWindowDimensions() < 768 ? 'menu-icons-mobile': 'menu-icons'} icon={basketballOutline} size='small'></IonIcon>	
 									<IonLabel className={sidebar ? 'menu-labels' : 'menu-labels collapsed' }>Lorem</IonLabel>
 								</IonButton>
@@ -175,6 +167,36 @@ const Dashboard = () => {
 					</aside>
 					
 						<IonGrid id='item-grid'>
+							{tab1 && <TabOne />}
+							{tab2 && <TabTwo />}
+								
+							{/* <IonRow id='item-main-row'> 
+								<IonCol id='layout-col'>
+									
+									<div>
+										<IonTitle>
+											Layout box
+											<IonIcon className='expand-btn' icon={expandOutline} size='small'></IonIcon>
+										</IonTitle>
+									</div>
+									
+									<IonRow id='layout-items-row'>
+
+										{items}
+										
+									</IonRow>
+								</IonCol>
+
+								<IonCol id='media-col'>
+									<div>
+										<IonTitle>
+											Media box?
+											<IonIcon className='expand-btn' icon={expandOutline} size='small'></IonIcon>
+										</IonTitle>
+									</div>
+								</IonCol>
+							</IonRow>
+						*/}
 							
 						</IonGrid>
 								
