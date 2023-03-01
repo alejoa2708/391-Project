589b04f4db4fc59484cb6bfe7e8bc96c9f8e6746
tadrosv <81656715+tadrosv@users.noreply.github.com>
2023-03-01 13:37:50 -0700

    Created Date table and inserts

---
+++ 391_Server/sql/INSERT_DATE.sql
@@ -0,0 +1,135 @@
+INSERT INTO Date (Date_ID, Year, Term) VALUES (1, 1990, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (2, 1990, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (3, 1990, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (4, 1991, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (5, 1991, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (6, 1991, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (7, 1992, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (8, 1992, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (9, 1992, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (10, 1993, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (11, 1993, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (12, 1993, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (13, 1994, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (14, 1994, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (15, 1994, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (16, 1995, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (17, 1995, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (18, 1995, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (19, 1996, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (20, 1996, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (21, 1996, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (22, 1997, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (23, 1997, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (24, 1997, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (25, 1998, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (26, 1998, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (27, 1998, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (28, 1999, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (29, 1999, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (30, 1999, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (31, 2000, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (32, 2000, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (33, 2000, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (34, 2001, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (35, 2001, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (36, 2001, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (37, 2002, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (38, 2002, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (39, 2002, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (40, 2003, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (41, 2003, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (42, 2003, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (43, 2004, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (44, 2004, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (45, 2004, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (46, 2005, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (47, 2005, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (48, 2005, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (49, 2006, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (50, 2006, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (51, 2006, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (52, 2007, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (53, 2007, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (54, 2007, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (55, 2008, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (56, 2008, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (57, 2008, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (58, 2009, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (59, 2009, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (60, 2009, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (61, 2010, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (62, 2010, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (63, 2010, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (64, 2011, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (65, 2011, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (66, 2011, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (67, 2012, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (68, 2012, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (69, 2012, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (70, 2013, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (71, 2013, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (72, 2013, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (73, 2014, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (74, 2014, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (75, 2014, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (76, 2015, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (77, 2015, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (78, 2015, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (79, 2016, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (80, 2016, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (81, 2016, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (82, 2017, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (83, 2017, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (84, 2017, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (85, 2018, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (86, 2018, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (87, 2018, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (88, 2019, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (89, 2019, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (90, 2019, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (91, 2020, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (92, 2020, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (93, 2020, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (94, 2021, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (95, 2021, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (96, 2021, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (97, 2022, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (98, 2022, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (99, 2022, 3);
+
+INSERT INTO Date (Date_ID, Year, Term) VALUES (100, 2023, 1);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (101, 2023, 2);
+INSERT INTO Date (Date_ID, Year, Term) VALUES (102, 2023, 3);
