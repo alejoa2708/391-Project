8a2bfada87514609f7e57c85b6bb70ab13f3ac40
aarona2708 <aaronalejo2708@gmail.com>
2023-03-02 21:52:37 -0700

    edited date sem and year attributes

--- 391_Server/sql/391_TABLES.sql
+++ 391_Server/sql/391_TABLES.sql
@@ -37,14 +37,16 @@ CREATE TABLE Section (
 	c_id INT,
 	ts_id INT,
 	i_id INT,
+	date_id INT,
 	semester CHAR(3) NOT NULL,
 	year INT NOT NULL,
-	capacity INT NOT NULL  ,
-	enrolled INT NOT NULL ,
+	capacity INT NOT NULL,
+	enrolled INT NOT NULL,
 	PRIMARY KEY (sec_id, c_id, ts_id, i_id, semester, year),
 	FOREIGN KEY (c_id) REFERENCES Course(c_id),
 	FOREIGN KEY (ts_id) REFERENCES Timeslot(ts_id),
 	FOREIGN KEY (i_id) REFERENCES Instructor(i_id),
+	FOREIGN KEY (date_id) REFERENCES Date(date_id),
 	CONSTRAINT CHK_ENR CHECK (capacity<=30)
 );
 
@@ -72,7 +74,7 @@ CREATE TABLE Prereq (
 
 
 CREATE TABLE Date (
-  Date_ID INT PRIMARY KEY,
-  Year INT CHECK (Year BETWEEN 1990 AND 2023),
-  Term INT CHECK (Term BETWEEN 1 AND 3)
+	date_id INT PRIMARY KEY,
+	semester CHAR(3) NOT NULL,
+	year INT NOT NULL,
 );
