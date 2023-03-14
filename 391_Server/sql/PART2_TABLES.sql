d6ddb35b19ed76cfe327ce4b104228f9ec8f3306
Benji <101489483+BenjaminKwon@users.noreply.github.com>
2023-03-14 12:54:35 -0600

    Update PART2_TABLES.sql

--- 391_Server/sql/PART2_TABLES.sql
+++ 391_Server/sql/PART2_TABLES.sql
@@ -5,30 +5,31 @@ Drop table Date;
 
 CREATE TABLE Date (
 	date_id INT PRIMARY KEY,
-	semester CHAR(3) NOT NULL,
-	year INT NOT NULL,
+	semester CHAR(3),
+	year INT,
 );
 
 CREATE TABLE Instructor (
 	ins_id INT PRIMARY KEY,
-	Fname CHAR(20) NOT NULL,
-    Lname CHAR(20) NOT NULL,
-    title CHAR(20) NOT NULL,
-    dept  CHAR(20) NOT NULL,
-    gender CHAR(1) NOT NULL,
+	Fname CHAR(20),
+    Lname CHAR(20),
+    title CHAR(20),
+    dept  CHAR(20),
+    gender CHAR(1),
 );
 
 CREATE TABLE Course (
 	course_id INT PRIMARY KEY,
-	Cname     CHAR(10) NOT NULL,
-	dept      CHAR(20) NOT NULL,
-    credits   INT NOT NULL
+	Cname     CHAR(10),
+	dept      CHAR(20),
+    credits   INT
 );
 
 CREATE TABLE Fact (
     ins_id INT NOT NULL,
     date_id INT NOT NULL,
     course_id INT NOT NULL,
+    count INT,
     FOREIGN KEY (ins_id) REFERENCES Instructor(ins_id),
     FOREIGN KEY (date_id) REFERENCES Date(date_id),
     FOREIGN KEY (course_id) REFERENCES Course(course_id)
